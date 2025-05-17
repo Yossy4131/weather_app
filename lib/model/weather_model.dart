fcf7402a13ee2a5891347668f864679c8f9ada54
Yossy41 <y.yoshida1031@gmail.com>
2025-05-17 21:08:13 +0900

    fix WeatherDate class

--- lib/model/weather_model.dart
+++ lib/model/weather_model.dart
@@ -4,14 +4,28 @@ part 'weather_model.freezed.dart';
 part 'weather_model.g.dart';
 
 @freezed
+@JsonSerializable(explicitToJson: true)
 class WeatherData with _$WeatherData {
-  @JsonSerializable(explicitToJson: true)
+  const WeatherData._();
+
   const factory WeatherData({
-  required List<String> time,
-  @JsonKey(name: 'temperature_2m_max') required List<double> temperature2mMax,
-  @JsonKey(name: 'temperature_2m_min') required List<double> temperature2mMin,
-  @JsonKey(name: 'precipitation_probability_max') required List<int> precipitationProbabilityMax,
-}) = _WeatherData;
+    required List<String> time,
+    required List<double> temperature2mMax,
+    required List<double> temperature2mMin,
+    required List<int> precipitationProbabilityMax,
+  }) = _WeatherData;
+
+  @override
+  @JsonKey(name: 'temperature_2m_max')
+  List<double> get temperature2mMax;
+
+  @override
+  @JsonKey(name: 'temperature_2m_min')
+  List<double> get temperature2mMin;
+
+  @override
+  @JsonKey(name: 'precipitation_probability_max')
+  List<int> get precipitationProbabilityMax;
 
   factory WeatherData.fromJson(Map<String, dynamic> json) =>
       _$WeatherDataFromJson(json);
