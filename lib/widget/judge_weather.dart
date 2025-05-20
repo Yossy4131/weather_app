ff468da6c2021889ad933a48a04d08929811c3f0
Yossy41 <y.yoshida1031@gmail.com>
2025-05-20 22:03:31 +0900

    add more wether judge

--- lib/widget/judge_weather.dart
+++ lib/widget/judge_weather.dart
@@ -11,9 +11,16 @@ class JudgeWeather {
     if (precipitationProbabilityMax >= 70) {
       weatherText = '雨';
       weatherIcon = Icon(WeatherIcons.rain, size: size, color: Colors.blue);
-    } else if (precipitationProbabilityMax >= 30) {
+    } else if (precipitationProbabilityMax >= 50) {
       weatherText = '曇り';
       weatherIcon = Icon(WeatherIcons.cloud, size: size, color: Colors.grey);
+    } else if (precipitationProbabilityMax >= 30) {
+      weatherText = '曇り';
+      weatherIcon = Icon(
+        WeatherIcons.day_cloudy,
+        size: size,
+        color: Colors.grey,
+      );
     } else {
       weatherText = '晴れ';
       weatherIcon = Icon(
